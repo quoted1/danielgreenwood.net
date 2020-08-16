8a94b697e75fee3d70e5d3205d6dd634c3d830ee
Daniel Greenwood <dangreenwood@zoho.com>
2020-08-16 22:10:18 +0100

    sorting out to be published on IONOS
    sorted the links and buttons on the header
    added the neccessary pages, about me/coming soon
    finished the slicksliders
    need to add the content to the new pages

--- Front End/Javascript/Index.js
+++ Front End/Javascript/Index.js
@@ -6,16 +6,46 @@
 //}
 
 
-$('.media_carousel').slick({
+$('.slick_H').slick({
     centerMode: true,
-    centerPadding: '250px',
     slidesToShow: 1,
     arrows:true,
     dots: true,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 6000,
+    autoplaySpeed: 5400,
     speed:1400,
+    variableWidth: true,
+    adaptiveHeight: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 3
+                }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 1
+                }
+        }
+    ]
+});
+$('.slick_V').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    arrows:false,
+    dots: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5400,
+    speed:1400,
+    vertical: true,
     responsive: [
         {
             breakpoint: 768,
