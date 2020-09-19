175d97c41c47d8e1624baf1d2af1078e3ddde4df
Daniel Greenwood <dangreenwood@zoho.com>
2020-09-19 15:35:30 +0100

    Moved the header/navigation bar into an external php script
    This means I only have to change one file for the header to be changed on all pages

--- Front End/About_Me.php
+++ Front End/About_Me.php
@@ -26,30 +26,7 @@
 <!-- end of scripts -->
 
 <body id="bodystyle_03">
-    <!--Start of NavBar-->
-    <nav class="navbar navbar-expand-sm header fixed-top">
-        <a class="navbar-brand hText align-items-center" href="index.php">
-            <span class="ztext_t_sm" >danielgreenwood.net</span>
-        </a>
-        <div id="navbarSupportedContent" class="ml-auto">
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown navItem">
-                    <a class="nav-link dropdown-toggle hText ztext_p_md" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My work</a>
-                    <div class="dropdown-menu dropdown-menu-right dropdownParent" aria-labelledby="navbarDropdown">
-                        <a class="ztext_p_sm" href="ran_Parallax.php">Parallax Effect</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="ztext_p_sm" href="Coming_Soon.php">Coming soon</a>
-                    </div>
-                </li>
-
-                <li class="nav-item navItem">
-                    <a class="nav-link hText ztext_p_md" href="About_Me.php" id="" role="button">About</a>
-                </li>
-               
-            </ul>
-        </div>
-    </nav>
-       <!--End of NavBar-->
+ <?php include 'Scripts/php/pageParts/header.php'; ?>
     
     <div class="container below-header-far">
         <div class="row">
--- Front End/Database_Handling.php
+++ Front End/Database_Handling.php
@@ -13,33 +13,13 @@
     <link rel="stylesheet" type="text/css" href="Scripts/CSS/General.css" />
     <link rel="stylesheet" type="text/css" href="Scripts/CSS/ztext_styles.css" />
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link src="packages/jquery-ui/jquery-ui.js"></script>
+    <link>
 
 </head>
 <body id="bodystyle_05">
  <!--Start of NavBar-->
-    <nav class="navbar navbar-expand-sm header fixed-top">
-        <a class="navbar-brand hText align-items-center" href="index.php">
-            <span class="ztext_t_sm" >danielgreenwood.net</span>
-        </a>
-        <div id="navbarSupportedContent" class="ml-auto">
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown navItem">
-                    <a class="nav-link dropdown-toggle hText ztext_p_md" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My work</a>
-                    <div class="dropdown-menu dropdown-menu-right dropdownParent" aria-labelledby="navbarDropdown">
-                        <a class="ztext_p_sm" href="ran_Parallax.php">Parallax Effect</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="ztext_p_sm" href="Coming_Soon.php">Coming soon</a>
-                    </div>
-                </li>
-
-                <li class="nav-item navItem">
-                    <a class="nav-link hText ztext_p_md" href="About_Me.php" id="" role="button">About</a>
-                </li>
-               
-            </ul>
-        </div>
-    </nav>
-<!--End of NavBar-->
+ <?php include 'Scripts/php/pageParts/header.php'; ?>
     <div class="container">
         <form class="below-header-close ">
             <div class="col-lg">
@@ -64,11 +44,9 @@
 
 </body>
 <!-- scripts -->
-    <script src="Scripts/php/sqlQuery.php"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="packages/jquery-ui/jquery-ui.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <!-- <script src="Scripts/php/dbh.php"></script> This line is hidden until the db connection is linked externally -->
 <!-- end of scripts -->
 </html>
