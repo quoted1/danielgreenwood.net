c3a73a67b4d11f2b624dcea50c60423d87077bc3
Daniel Greenwood <dangreenwood@zoho.com>
2020-10-27 14:13:12 +0000

    Revert "added login form to database_handling.php"
    This reverts commit cf521c159e211cf61f530909b3237f13f596b68c.

--- Front End/Scripts/php/sqlQuery.php
+++ Front End/Scripts/php/sqlQuery.php
@@ -7,21 +7,25 @@
         echo "<p>Email - ".$row[Email]." | Username - ".$row[Username]."</p>";
     }
 
+    $query_02 = "SELECT * FROM Users WHERE Username = 'dgwood'";
+
     $query_03 = "INSERT INTO `Users` (`Email`, `Username`, `Password`) VALUES('dangreenwood@zoho.com', 'dgwood', 'Password123')";
 
     $query_04 = "UPDATE Users SET Email = 'd.greenwood@pitchero.com' WHERE Username = 'dgwood'";
 
-    function chk_username(){
-        $stmt = $conn->prepare("SELECT * FROM Users WHERE Username = ?");
-        $stmt->bind_param('s', $_POST['emailUsername']);
-        $stmt->execute();
-        $stmt->store_result();
-        $stmt->bind_result($cid, $cname, $name, $adr, $city, $pcode, $country);
-        $stmt->fetch();
-        $stmt->close();
+function chk_dgwood(){
+    $result = mysqli_query($conn, $query_02);
+    $row = mysqli_fetch_array($result);
+    if ($row[Email] != "") {
+        if ($row[Email] == "dangreenwood@zoho.com") {
+            echo "<p>Email = dangreenwood@zoho.com, Updating to d.greenwood@pitchero.com</p>";
+            mysqli_query($conn, $query_04);
+        } else {
+            echo "<p>Email = d.greenwood@pitchero.com, no need to change anything</p>";
+        }
+    } else {
+        echo "<p>No User called <strong>dgwood</strong>, adding now</p>";
+        mysqli_query($conn, $query_03);
     }
-
-    function log_in(){
-        echo $_POST['emailUsername'];
-    }
+}
 ?>
